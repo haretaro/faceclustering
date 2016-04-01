344d197ff5164ded168e587522b273278931640b
haretaro <frontend.1000@gmail.com>
2016-04-01 21:18:20 +0900

    解像度の高い方のデータセットを読み込むように修正

--- faceclustering.m
+++ faceclustering.m
@@ -1,11 +1,13 @@
-cluster_num = 4;
-iter_num = 10;
-pictsize = [40,40];
-X = readImages('40x40/data',300,pictsize);
+cluster_num = 2;
+iter_num = 20;
+%pictsize = [40,40];
+%X = readImages('40x40/data',300,pictsize);
+pictsize = [240,320];
+X = readImages('motion/data',300,pictsize);
 m = size(X,1);
 init_y = randi(cluster_num,m);
 init_y = init_y(:,1);
-[y,error] = kmeans(X,init_y, cluster_num, iter_num);
+[y,error] = kmeans(X,init_y, cluster_num, iter_num, 2);
 for k=1:cluster_num
   c = X(y==k,:);
   size(c)
